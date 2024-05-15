10868571f5ccc562f71725e706ffe0c840e7a2fa
Ashley Johnson <61059402+PapaRascal2020@users.noreply.github.com>
2024-05-15 17:44:03 +0100

    Update EloquentAiServiceProvider.php
    Removed comments

--- src/EloquentAiServiceProvider.php
+++ src/EloquentAiServiceProvider.php
@@ -11,23 +11,10 @@ class EloquentAiServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'eloquent-ai');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'eloquent-ai');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('eloquent-ai.php'),
             ], 'config');
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/eloquent-ai'),
-            ], 'lang');*/
         }
     }
 
